f2d5a3eb5ee1f79e78fdc060b214f9030e3a445e
Raghavendra <nraghavendra89@gmail.com>
2024-11-26 19:13:37 +0530

    Remove example files.

--- _stacks/assets/js/first-example.js
+++
@@ -1,5 +0,0 @@
-animatorModule.setAnimationSpeed(1.5);
-const stack1 = new Stack([1, 2, 3], {name: 'Input Stack', description: 'This is a small description!'});
-stack1.push(9);
-stack1.push(1);
-stack1.pop();
